b9154576d2e20b8d03cfa7dd9502ffb03b5cf2b7
DangQuangVi <vidangquang1203@gmail.com>
2025-04-03 12:29:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -80,8 +80,10 @@ def main(args: argparse.Namespace) -> None:
     # set device
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Device: {}".format(device))
-    # if device == "cpu": # Không có GPU
-    #     raise ValueError("GPU not detected!")
+    if device == "cuda":
+        torch.cuda.set_per_process_memory_fraction(0.8, device=0)
+    if device == "cpu": # Không có GPU
+        raise ValueError("GPU not detected!")
 
     # define model architecture
     model = get_model(model_config, device)
